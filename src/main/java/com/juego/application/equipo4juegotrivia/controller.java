a1e6753f8c896a7ab0ec3b6588d713f0a13cffdc
Brandon Cueva <brandon_cueva@unitec.edu>
2025-08-20 17:44:12 -0600

    feat: Pantalla menu principal con botones Jugar y Salir

--- src/main/java/com/juego/application/equipo4juegotrivia/controller.java
+++ src/main/java/com/juego/application/equipo4juegotrivia/controller.java
@@ -1,14 +1,38 @@
 package com.juego.application.equipo4juegotrivia;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void jugar(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("pregunta-view.fxml")));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 800, 600));
+            stage.setTitle("Juego de Trivia");
+            stage.show();
+
+            // Cerrar el menú actual - FORMA CORRECTA
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void salir() {
+        Platform.exit();
     }
 }
